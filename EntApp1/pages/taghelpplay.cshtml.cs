98b9924e32f8f81b12b88ad7cb5f43652c729f6c
Leo Gavin <0442682@gmail.com>
2019-02-10 20:34:45 +0000

    latest updates

---
+++ EntApp1/pages/taghelpplay.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntApp1.pages
+{
+    public class taghelpplayModel : PageModel
+    {
+        [BindProperty]
+
+        public int Number1 { get; set; }
+
+        public int? Number2 { get; set; }
+
+        public double Number3 { get; set; }
+
+        public double? Number4 { get; set; }
+
+        public decimal Number5 { get; set; }
+
+        public decimal? Number6 { get; set; }
+
+        public float Number7 { get; set; }
+
+        public float? Number8 { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime timeofday { get; set; }
+
+        public DateTime? timeofday1 { get; set; }
+
+        public bool IsContactAllowed { get; set; }
+
+        [BindProperty]
+        public string Season { get; set; }
+
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            if (Request.Form["Season"] == "Spring")
+                Message = string.Format("You have picked Spring");
+            if (Request.Form["Season"] == "Summer")
+                Message = string.Format("You have picked Summer");
+            if (Request.Form["Season"] == "Autumn")
+                Message = string.Format($"You have picked Autumn");
+            if (Request.Form["Season"] == "Winter")
+                Message = string.Format($"You have picked Winter");
+        }
+
+
+
+
+
+
+
+
+
+       
+    }
+}
---
+++ EntApp1/pages/ws1b.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntApp1.pages
+{
+    public class ws1bModel : PageModel
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            
+
+
+        }
+
+        public void OnPost(int number1, int number2)
+        {
+            int added = number1 + number2;
+            int subtracted = number1 - number2;
+            int multiplied = number1 * number2;
+            int divided = number1 / number2;
+
+            if (Request.Form["action"] == "add")
+                Message = string.Format($"The sum of {number1} and {number2} is {added}");
+            if (Request.Form["action"] == "subtract")
+                Message = string.Format($"{number1} minus {number2} is {subtracted}");
+            if (Request.Form["action"] == "multiply")
+                Message = string.Format($"{number1} multiplied by {number2} is {multiplied}");
+            if (Request.Form["action"] == "divide")
+                Message = string.Format($"{number1} divided by {number2} is {divided}");
+        }
+    }
+}
